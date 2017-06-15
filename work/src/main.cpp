e9428edc210d96fd0850d2cc9844c8c173d982eb
Orion Holder-Monk <orionholdermonk@gmail.com>
2017-06-15 14:56:37 +1200

    Attempting to load model and render

--- work/src/main.cpp
+++ work/src/main.cpp
@@ -36,6 +36,8 @@ void renderCube();
 void renderPlane();
 
 World *g_world = nullptr;
+
+vector<float> humanModel;
 //****************************************************************************************************************************
 //************************************** Set Up Camera System ****************************************************************
 //****************************************************************************************************************************
@@ -428,7 +430,7 @@ int main() {
 	g_world = new World();
 	g_world->init(-10.0f, -10.0f, 10.0f, 10.0f);
 
-
+	humanModel = g_world->getAgents()[0].getModel();
 
 
 
@@ -574,18 +576,18 @@ int main() {
 			renderSphere();
 		}
 
-		vector<ParkObject> parkObjects = g_world->getParkObjects();
-		for (int i = 0; i < parkObjects.size(); i++) {
-			vec2 pos = parkObjects[i].getPosition();
-			model = glm::mat4();
-			model = glm::translate(model, glm::vec3(
-				pos.x,
-				-2.0f,
-				pos.y
-			));
-			pbrShader.setMat4("model", model);
-			renderSphere();
-		}
+		// vector<ParkObject> parkObjects = g_world->getParkObjects();
+		// for (int i = 0; i < parkObjects.size(); i++) {
+		// 	vec2 pos = parkObjects[i].getPosition();
+		// 	model = glm::mat4();
+		// 	model = glm::translate(model, glm::vec3(
+		// 		pos.x,
+		// 		-2.0f,
+		// 		pos.y
+		// 	));
+		// 	pbrShader.setMat4("model", model);
+		// 	renderSphere();
+		// }
 		/*for (int row = 0; row < nrRows; ++row)
 		{
 			pbrShader.setFloat("metallic", (float)row / (float)nrRows);
@@ -633,7 +635,7 @@ int main() {
 		model = glm::scale(model, glm::vec3(0.5f));
 		pbrShader.setMat4("model", model);
 		
-		renderSphere();
+		//renderSphere();
 
 
 		// render skybox (render as last to prevent overdraw)
@@ -808,76 +810,79 @@ void renderSphere()
 		glGenBuffers(1, &vbo);
 		glGenBuffers(1, &ebo);
 
-		std::vector<glm::vec3> positions;
-		std::vector<glm::vec2> uv;
-		std::vector<glm::vec3> normals;
-		std::vector<unsigned int> indices;
+		// std::vector<glm::vec3> positions;
+		// std::vector<glm::vec2> uv;
+		// std::vector<glm::vec3> normals;
+		// std::vector<unsigned int> indices;
 
-		const unsigned int X_SEGMENTS = 64;
-		const unsigned int Y_SEGMENTS = 64;
-		const float PI = 3.14159265359;
-		for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
-		{
-			for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
-			{
-				float xSegment = (float)x / (float)X_SEGMENTS;
-				float ySegment = (float)y / (float)Y_SEGMENTS;
-				float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
-				float yPos = std::cos(ySegment * PI);
-				float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+		// const unsigned int X_SEGMENTS = 64;
+		// const unsigned int Y_SEGMENTS = 64;
+		// const float PI = 3.14159265359;
+		// for (unsigned int y = 0; y <= Y_SEGMENTS; ++y)
+		// {
+		// 	for (unsigned int x = 0; x <= X_SEGMENTS; ++x)
+		// 	{
+		// 		float xSegment = (float)x / (float)X_SEGMENTS;
+		// 		float ySegment = (float)y / (float)Y_SEGMENTS;
+		// 		float xPos = std::cos(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
+		// 		float yPos = std::cos(ySegment * PI);
+		// 		float zPos = std::sin(xSegment * 2.0f * PI) * std::sin(ySegment * PI);
 
-				positions.push_back(glm::vec3(xPos, yPos, zPos));
-				uv.push_back(glm::vec2(xSegment, ySegment));
-				normals.push_back(glm::vec3(xPos, yPos, zPos));
-			}
-		}
+		// 		positions.push_back(glm::vec3(xPos, yPos, zPos));
+		// 		uv.push_back(glm::vec2(xSegment, ySegment));
+		// 		normals.push_back(glm::vec3(xPos, yPos, zPos));
+		// 	}
+		// }
 
-		bool oddRow = false;
-		for (int y = 0; y < Y_SEGMENTS; ++y)
-		{
-			if (!oddRow) // even rows: y == 0, y == 2; and so on
-			{
-				for (int x = 0; x <= X_SEGMENTS; ++x)
-				{
-					indices.push_back(y       * (X_SEGMENTS + 1) + x);
-					indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
-				}
-			}
-			else
-			{
-				for (int x = X_SEGMENTS; x >= 0; --x)
-				{
-					indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
-					indices.push_back(y       * (X_SEGMENTS + 1) + x);
-				}
-			}
-			oddRow = !oddRow;
-		}
-		indexCount = indices.size();
+		// bool oddRow = false;
+		// for (int y = 0; y < Y_SEGMENTS; ++y)
+		// {
+		// 	if (!oddRow) // even rows: y == 0, y == 2; and so on
+		// 	{
+		// 		for (int x = 0; x <= X_SEGMENTS; ++x)
+		// 		{
+		// 			indices.push_back(y       * (X_SEGMENTS + 1) + x);
+		// 			indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+		// 		}
+		// 	}
+		// 	else
+		// 	{
+		// 		for (int x = X_SEGMENTS; x >= 0; --x)
+		// 		{
+		// 			indices.push_back((y + 1) * (X_SEGMENTS + 1) + x);
+		// 			indices.push_back(y       * (X_SEGMENTS + 1) + x);
+		// 		}
+		// 	}
+		// 	oddRow = !oddRow;
+		// }
+		// indexCount = indices.size();
 
-		std::vector<float> data;
-		for (int i = 0; i < positions.size(); ++i)
-		{
-			data.push_back(positions[i].x);
-			data.push_back(positions[i].y);
-			data.push_back(positions[i].z);
-			if (uv.size() > 0)
-			{
-				data.push_back(uv[i].x);
-				data.push_back(uv[i].y);
-			}
-			if (normals.size() > 0)
-			{
-				data.push_back(normals[i].x);
-				data.push_back(normals[i].y);
-				data.push_back(normals[i].z);
-			}
-		}
+		// std::vector<float> data;
+		// for (int i = 0; i < positions.size(); ++i)
+		// {
+		// 	data.push_back(positions[i].x);
+		// 	data.push_back(positions[i].y);
+		// 	data.push_back(positions[i].z);
+		// 	if (uv.size() > 0)
+		// 	{
+		// 		data.push_back(uv[i].x);
+		// 		data.push_back(uv[i].y);
+		// 	}
+		// 	if (normals.size() > 0)
+		// 	{
+		// 		data.push_back(normals[i].x);
+		// 		data.push_back(normals[i].y);
+		// 		data.push_back(normals[i].z);
+		// 	}
+		// }
+
+
+
 		glBindVertexArray(sphereVAO);
 		glBindBuffer(GL_ARRAY_BUFFER, vbo);
-		glBufferData(GL_ARRAY_BUFFER, data.size() * sizeof(float), &data[0], GL_STATIC_DRAW);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, humanModel.size() * sizeof(float), &humanModel[0], GL_STATIC_DRAW);
+		// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		// glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
 		float stride = (3 + 2 + 3) * sizeof(float);
 		glEnableVertexAttribArray(0);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)0);
@@ -888,7 +893,8 @@ void renderSphere()
 	}
 
 	glBindVertexArray(sphereVAO);
-	glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
+	glDrawArrays(GL_TRIANGLES,0,20);
+	//glDrawElements(GL_TRIANGLE_STRIP, indexCount, GL_UNSIGNED_INT, 0);
 }
 
 // renderCube() renders a 1x1 3D cube in NDC.
